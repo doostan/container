36b17b3b057710ba3ddc55667c8a9c0e646f90aa
saeedshpour <saeedshpour@gmail.com>
2015-03-07 22:29:34 +0330

    add Container class

---
+++ Doostan/Container/Container.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Doostan\Container component is a sample Dependency Injection Container.
+ * 
+ * @link        https://github.com/doostan/container
+ * @license     http://opensource.org/licenses/MIT MIT license
+ * @copyright   Copyright (c) 2015 https://github.com/doostan
+ * @author      doostan doostan.github@gmail.com
+ */
+
+namespace Doostan\Container;
+
+class Container
+{
+    
+}
