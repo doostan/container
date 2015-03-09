f307b7f804ca3bd19026f6f6fd3cc02f910ce623
doostan <doostan.github@gmail.com>
2015-03-09 22:05:51 +0330

    container class

--- src/Doostan/Container/Container.php
+++ src/Doostan/Container/Container.php
@@ -12,5 +12,59 @@ namespace Doostan\Container;
 
 class Container
 {
+    /**
+     * @var array holds all services
+     */
+    protected $services;
     
+    /**
+     * constructor.
+     */
+    private function __construct()
+    {
+        $this->services=array();
+    }
+    
+    /**
+     * sets a service.
+     *
+     * @param string $id
+     * @param closure|object $service
+     * @return void
+     */
+    public function setService($id, $service)
+    {
+        if(is_callable($service)) {
+            $this->services[$id] = function($container) use ($service) {
+                static $ser=null;
+                if ($ser === null) {
+                    $ser = $service($container);
+                }
+                return $ser;
+            };
+        } elseif (is_object($service)) {
+            $this->services[$id] = $service;
+        }
+    }
+
+    /**
+     * returns a service.
+     *
+     * @param string $id
+     * @return mixed
+     * @throws Exception
+     */
+    public function getService($id)
+    {
+        if (array_key_exists($id, $this->services)) {
+            if (is_callable($this->services[$id])) {
+                $ser = $this->services[$id];
+                return $ser($this);
+            } else {
+                return $this->services[$id];
+            }
+        } else {
+            throw new \Exception($id.' service not found');
+        }
+    }
 }
